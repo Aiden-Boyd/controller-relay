6008723d71cb407df9a773483ceb267bfdd1b1e0
Aiden <63018437+Aiden-Boyd@users.noreply.github.com>
2026-08-28 10:27:57 -0400

    Track Satellite control and data ports

--- DishIOS/Models/SatelliteHost.swift
+++ DishIOS/Models/SatelliteHost.swift
@@ -6,6 +6,8 @@ struct SatelliteHost: Identifiable, Hashable {
     let name: String
     let endpoint: NWEndpoint
     let pairingPort: UInt16
+    let httpPort: UInt16
+    let udpPort: UInt16
 
     var id: String { machineID }
 }
